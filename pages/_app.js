b2dcef118ce352eb358a33eaa1cb4edce7547946
Kirby McKenzie <kirby@workplacearcade.com>
2020-11-02 20:41:34 +1100

    Add Chakra UI and replace index.js with small chakra sample

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
 import React from "react";
 import "../styles/globals.css";
 import PropTypes from "prop-types";
+import { ChakraProvider } from "@chakra-ui/core";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
 MyApp.propTypes = {
