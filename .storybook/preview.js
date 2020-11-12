213394c055e407e63e129592219840c2ad30a782
Kirby McKenzie <kirby@workplacearcade.com>
2020-11-12 21:15:13 +1100

    Update storybook to use Chakra theme

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,13 @@
-export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+import React from "react";
+import { ChakraProvider, CSSReset } from "@chakra-ui/core";
+
+const withThemeProvider = (Story, context) => {
+  return (
+    <ChakraProvider>
+      <CSSReset />
+      <Story {...context} />
+    </ChakraProvider>
+  );
 };
+
+export const decorators = [withThemeProvider];
