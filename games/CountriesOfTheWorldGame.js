184cc826b3025c6a2643c69c21bc4cf8296a3d9e
Kirby McKenzie <kirby@workplacearcade.com>
2020-11-22 13:32:02 +1100

    Update CountriesOfTheWorldGame to render the world map with a better aspect ratio, and update the map to highlight on hover

--- games/CountriesOfTheWorldGame.js
+++ games/CountriesOfTheWorldGame.js
@@ -4,13 +4,17 @@ import { SVGMap } from "react-svg-map";
 import World from "@svg-maps/world";
 
 const CountriesOfTheWorldGame = () => (
-  <Box width="100%">
-    <Heading m={10} textAlign="center">
+  <Box width="100%" height="100vh" backgroundColor="#F5F5F5">
+    <Heading px={10} py={8} textAlign="center">
       {"Countries of the World Quiz"}
     </Heading>
 
     <Box width="100%" textAlign="center">
-      <SVGMap map={World} />
+      <SVGMap
+        map={World}
+        className="countries-of-world"
+        locationClassName="highlight-on-hover"
+      />
     </Box>
   </Box>
 );
