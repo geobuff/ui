f93be9b4b419b427510e2fbedb9955c26738aee8
Kirby McKenzie <kirby@workplacearcade.com>
2020-12-06 14:00:36 +1100

    Add sheet modal to countries of the world quiz for  mobile breakpoints

--- games/CountriesOfTheWorldGame/CountriesOfTheWorldGame.js
+++ games/CountriesOfTheWorldGame/CountriesOfTheWorldGame.js
@@ -1,42 +1,63 @@
-import React from "react";
-import { Box, Heading, Text } from "@chakra-ui/core";
+import React, { useState } from "react";
+import { Box, Heading, Divider, useBreakpointValue } from "@chakra-ui/core";
+
 import { SVGMap } from "react-svg-map";
 import World from "@svg-maps/world";
 import Sheet from "react-modal-sheet";
 
-const CountriesOfTheWorldGame = () => (
-  <Box width="100%" height="100vh" backgroundColor="#F5F5F5">
-    <Heading px={10} py={8} textAlign="center">
-      {"Countries of the World Quiz"}
-    </Heading>
+const snapPoints = [600, 400, 200, 200];
+const initialSnap = snapPoints.length - 1;
 
-    <Box width="100%" textAlign="center">
-      <SVGMap
-        map={World}
-        className="countries-of-world"
-        locationClassName="highlight-on-hover"
-      />
+const CountriesOfTheWorldGame = () => {
+  const shouldShowSheet = useBreakpointValue({ base: true, lg: false });
+  const [isOpen, setIsOpen] = useState(true);
+
+  // Because we want the modal to stay open, this forces the
+  // modal to stay open even if it's forced closed by pesky users
+  const handleClose = () => {
+    setIsOpen(false);
+    setIsOpen(true);
+  };
+
+  return (
+    <Box width="100%" height="100vh" backgroundColor="#F5F5F5">
+      {!shouldShowSheet && (
+        <Heading px={10} py={8} textAlign="center">
+          {"Countries of the World Quiz"}
+        </Heading>
+      )}
+      <Box width="100%" pt={2} textAlign="center">
+        <SVGMap
+          map={World}
+          className="countries-of-world"
+          locationClassName="highlight-on-hover"
+        />
+      </Box>
+
+      {shouldShowSheet && (
+        <Box
+          as={Sheet}
+          isOpen={isOpen}
+          onClose={handleClose}
+          snapPoints={snapPoints}
+          initialSnap={initialSnap}
+          mt={11}
+        >
+          <Sheet.Container>
+            <Sheet.Header />
+            <Sheet.Content>
+              <Box m={5} textAlign="center">
+                <Heading size="lg" mt={0} mb={6}>
+                  {"Countries of the World Quiz"}
+                </Heading>
+                <Divider />
+              </Box>
+            </Sheet.Content>
+          </Sheet.Container>
+        </Box>
+      )}
     </Box>
-
-    <Sheet
-      isOpen
-      onClose={null}
-      snapPoints={[600, 400, 100, 0]}
-      initialSnap={1}
-    >
-      <Sheet.Container>
-        <Sheet.Header />
-        <Sheet.Content>
-          {
-            <Text>{"Hey haha"}</Text>
-            /* Your sheet content goes here */
-          }
-        </Sheet.Content>
-      </Sheet.Container>
-
-      {/* <Sheet.Backdrop /> */}
-    </Sheet>
-  </Box>
-);
+  );
+};
 
 export default CountriesOfTheWorldGame;
