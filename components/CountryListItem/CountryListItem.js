51590b131c3541a7a516a64395aa98a9fc0586f5
Kirby McKenzie <kirby@workplacearcade.com>
2020-12-08 20:53:59 +1100

    Update CountryListItem to accept country code and name and render the correct emoji flag

--- components/CountryListItem/CountryListItem.js
+++ components/CountryListItem/CountryListItem.js
@@ -1,18 +1,34 @@
 import React from "react";
-import { Flex, ListItem, Text } from "@chakra-ui/core";
+import PropTypes from "prop-types";
+import { Fade, Flex, ListItem, Text } from "@chakra-ui/core";
+import flag from "country-code-emoji";
+
 import Twemoji from "../Twemoji";
 
-const CountryListItem = () => {
+const CountryListItem = ({ code, name }) => {
+  const isValidCountryCode = code && code.length === 2;
   return (
     <ListItem listStyleType="none">
-      <Flex alignItems="center">
-        <Twemoji emoji="🇳🇿" />
-        <Text ml={2} fontWeight="600" fontSize={14}>
-          {"New Zealand"}
-        </Text>
-      </Flex>
+      <Fade in>
+        <Flex alignItems="center">
+          {isValidCountryCode && <Twemoji emoji={flag(code)} />}
+          <Text ml={2} fontWeight="600" fontSize={14}>
+            {name}
+          </Text>
+        </Flex>
+      </Fade>
     </ListItem>
   );
+};
+
+CountryListItem.propTypes = {
+  code: PropTypes.string,
+  name: PropTypes.string,
+};
+
+CountryListItem.defaultProps = {
+  code: "nz",
+  name: "New Zealand",
 };
 
 export default CountryListItem;
