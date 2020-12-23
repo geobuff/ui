e5f0894794d44c1dcfd5b3d46087b2e0558cb8af
Kirby McKenzie <kirby@workplacearcade.com>
2020-12-24 10:28:28 +1100

    Add simple GameInputBanner and add it to the CountriesOfTheWorldGame

---
+++ components/GameInputBanner/GameInputBanner.stories.js
@@ -0,0 +1,16 @@
+import React from "react";
+import GameInputBanner from "./GameInputBanner";
+
+export default {
+  title: "UI/GameInputBanner",
+  component: GameInputBanner,
+};
+
+const Template = (args) => <GameInputBanner {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  score: 69,
+  total: 193,
+  verb: "countries",
+};
