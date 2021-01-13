8e5667f3168a94149bca2c5335f2e469c5781d9c
ash.midgley <ashleymidgley@protonmail.com>
2021-01-13 21:40:05 +1300

    Add placeholder component for CountryResultsList.

--- components/CountryResultsListContainer/CountryResultsListContainer.js
+++ components/CountryResultsListContainer/CountryResultsListContainer.js
@@ -3,12 +3,17 @@ import useSWR from "swr";
 
 import { fetcher } from "../../helpers/fetcher";
 import CountryResultsList from "../CountryResultsList/CountryResultsList";
+import CountryResultsListPlaceholder from "../CountryResultsListPlaceholder/CountryResultsListPlaceholder";
 
 const CountryResultsListContainer = () => {
   const { data } = useSWR(
     `${process.env.NEXT_PUBLIC_API_URL}/countries`,
     fetcher
   );
+
+  if (!data) {
+    return <CountryResultsListPlaceholder />;
+  }
 
   return <CountryResultsList countriesByContinent={data} />;
 };
