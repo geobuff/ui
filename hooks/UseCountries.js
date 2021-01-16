63d568c8de681e5fd5dc16087abc869cccabaf6a
Kirby McKenzie <kirby@workplacearcade.com>
2021-01-16 14:12:56 +1100

    Update useCountries hook to use SWR logic

--- hooks/UseCountries.js
+++ hooks/UseCountries.js
@@ -1,17 +1,11 @@
-import { useEffect, useState } from "react";
-// import useSWR from "swr";
-
-// import { fetcher } from "../helpers/fetcher";
+import useSWR from "swr";
+import { fetcher } from "../helpers/fetcher";
 
 const useCountries = () => {
-  // const { data } = useSWR(
-  //   `${process.env.NEXT_PUBLIC_API_URL}/countries`,
-  //   fetcher
-  // );
-
-  const [isPending, setIsPending] = useState(true);
-  const [countriesByContinent, setCountriesByContinent] = useState([]);
-  const [allCountries, setAllCountries] = useState([]);
+  const { data } = useSWR(
+    `${process.env.NEXT_PUBLIC_API_URL}/countries`,
+    fetcher
+  );
 
   const flattenCountries = (countriesByContinent) => {
     let flattenedCountries = [];
@@ -28,22 +22,10 @@ const useCountries = () => {
     return flattenedCountries;
   };
 
-  useEffect(() => {
-    fetch(`${process.env.NEXT_PUBLIC_API_URL}/countries`)
-      .then((response) => response.json())
-      .then((data) => {
-        setCountriesByContinent(data);
-        setAllCountries(flattenCountries(data));
-        setIsPending(false);
-      });
-  }, []);
-
   return {
-    allCountries,
-    countriesByContinent,
-    isPending,
-    setAllCountries,
-    setCountriesByContinent,
+    allCountries: data ? flattenCountries(data) : [],
+    countriesByContinent: data || [],
+    isPending: !data,
   };
 };
 
