263524a0679d3b3448889a9649b9affdc9295745
Ash Midgley <ashleymidgley@protonmail.com>
2021-02-23 11:09:59 +1300

    Add LeaderboardTablePlaceholder.

---
+++ placeholders/LeaderboardTablePlaceholder/LeaderboardTablePlaceholder.js
@@ -0,0 +1,17 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Stack, Skeleton } from "@chakra-ui/core";
+
+const LeaderboardTablePlaceholder = ({ noOfLines }) => (
+  <Stack>
+    {[...Array(noOfLines)].map((_, i) => (
+      <Skeleton key={i} height="20px" />
+    ))}
+  </Stack>
+);
+
+LeaderboardTablePlaceholder.propTypes = {
+  noOfLines: PropTypes.number,
+};
+
+export default LeaderboardTablePlaceholder;
