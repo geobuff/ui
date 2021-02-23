e7a7cdbb2fd1a682dcb2936cc3999eadee627d2f
Kirby McKenzie <kirby@workplacearcade.com>
2021-02-23 21:18:37 +1100

    Update layout to not rely on vh

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useCallback } from "react";
-import { Box, Input } from "@chakra-ui/core";
 import { debounce } from "debounce";
+
+import { Box, Input, Text, Flex } from "@chakra-ui/core";
 import QuizListContainer from "../containers/QuizListContainer/QuizListContainer";
 
 const Home = () => {
@@ -17,12 +18,51 @@ const Home = () => {
   const handleDebounceChange = useCallback(debounce(onChange, 500), [onChange]);
 
   return (
-    <Box width="100%">
-      <Box width="50%" mx="auto" mt={10}>
-        <Input onChange={handleChange} placeholder="Enter quiz name..." />
-      </Box>
-      <QuizListContainer filter={filter} />
-    </Box>
+    <>
+      <style global jsx>{`
+        body {
+          background-color: #f0f0f0;
+        }
+      `}</style>
+
+      <Flex width="100%" mx="auto" position="relative" alignItems="flex-start">
+        <Box position="absolute" left="0" right="0" top="0">
+          <Box
+            width="100%"
+            height="35vh"
+            background="linear-gradient(90deg, #44D581 0%, #27AE60 100%)"
+          >
+            <Box padding={12}>
+              <Text fontSize="48px" fontWeight="black" lineHeight="100%">
+                {"Welcome to"}
+              </Text>
+              <Text fontSize="48px" fontWeight="black">
+                {"GeoBuff"}
+              </Text>
+            </Box>
+            {/* <Box position="absolute" right="20px" top="75px">
+              <Image height="300px" width="600px" src="/world-map.svg" />
+            </Box> */}
+          </Box>
+          <Box
+            mx="auto"
+            position="relative"
+            left={0}
+            right={0}
+            bottom="21px"
+            maxWidth={{ base: "80%", md: "60%" }}
+          >
+            <Input
+              width="100%"
+              size="lg"
+              onChange={handleChange}
+              placeholder="Enter quiz name..."
+            />
+          </Box>
+          <QuizListContainer filter={filter} />
+        </Box>
+      </Flex>
+    </>
   );
 };
 
