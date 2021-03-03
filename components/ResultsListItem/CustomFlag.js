36ef26145db781048a6c116aeb1a4da206a92e99
Ash Midgley <ashleymidgley@protonmail.com>
2021-03-04 09:46:06 +1300

    Add loading skeleton for custom flag.

--- components/ResultsListItem/CustomFlag.js
+++ components/ResultsListItem/CustomFlag.js
@@ -1,18 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
-import { Box } from "@chakra-ui/react";
+import { Box, Skeleton, Image } from "@chakra-ui/react";
 
-const CustomFlag = ({ url }) => (
-  <Box
-    height="18px"
-    width="24.5px"
-    borderRadius={4}
-    bgImage={`url('${url}')`}
-    bgPosition="center"
-    bgRepeat="no-repeat"
-    bgSize="cover"
-  ></Box>
-);
+const CustomFlag = ({ url }) => {
+  const [hasLoaded, setHasLoaded] = useState(false);
+
+  return (
+    <Box
+      borderRadius={4}
+      height="18px"
+      width="24.5px"
+      backgroundColor="transparent"
+    >
+      <Image
+        display={hasLoaded ? "flex" : "none"}
+        src={url}
+        alt={"Custom flag."}
+        borderRadius={4}
+        height="18px"
+        width="24.5px"
+        onLoad={() => setHasLoaded(true)}
+      />
+      <Skeleton
+        display={hasLoaded ? "none" : "flex"}
+        borderRadius={4}
+        height="18px"
+        width="24.5px"
+      />
+    </Box>
+  );
+};
 
 CustomFlag.propTypes = {
   url: PropTypes.string,
