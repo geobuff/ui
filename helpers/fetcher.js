a49b18db4cbb5bc73e23fa225a4a586ab228f755
Kirby McKenzie <kirby@workplacearcade.com>
2021-03-24 19:24:57 +1100

    Update authFetcher to use axios

--- helpers/fetcher.js
+++ helpers/fetcher.js
@@ -4,11 +4,10 @@ export const fetcher = (url) => axiosClient.get(url).then((res) => res.data);
 
 export const authFetcher = (url, token) => {
   const options = {
-    method: "GET",
     headers: new Headers({
       Authorization: `Bearer ${token}`,
     }),
   };
 
-  return fetch(url, options).then((res) => res.json());
+  return axiosClient.get(url, options).then((res) => res.data);
 };
