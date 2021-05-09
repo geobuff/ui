44a59a4aed9917f6e67a93e8d327fbbadfa3762e
Ash Midgley <ashleymidgley@protonmail.com>
2021-05-09 14:18:43 +1200

    REname component.

--- components/GameFlag/GameFlag.js
+++ components/GameFlag/GameFlag.js
@@ -3,28 +3,32 @@ import PropTypes from "prop-types";
 import { Box, Image } from "@chakra-ui/react";
 import { getFlagUrl } from "@geobuff/flags";
 
-const GameFlags = ({ code }) => (
-  <Box width="100%" height="100%">
-    <Image
-      src={getFlagUrl(code)}
-      width={{ base: "80%", md: "500px" }}
-      borderRadius={4}
-      position="absolute"
-      top="0"
-      left="0"
-      right="0"
-      bottom="0"
-      margin="auto"
-      pb="50px"
-    />
-  </Box>
-);
+const GameFlag = ({ code }) => {
+  console.log("Hi Daniel");
 
-GameFlags.propTypes = {
+  return (
+    <Box width="100%" height="100%">
+      <Image
+        src={getFlagUrl(code)}
+        width={{ base: "80%", md: "500px" }}
+        borderRadius={4}
+        position="absolute"
+        top="0"
+        left="0"
+        right="0"
+        bottom="0"
+        margin="auto"
+        pb="50px"
+      />
+    </Box>
+  );
+};
+
+GameFlag.propTypes = {
   code: PropTypes.string,
 };
-GameFlags.defaultProps = {
+GameFlag.defaultProps = {
   code: "nz",
 };
 
-export default GameFlags;
+export default GameFlag;
