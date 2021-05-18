181967767f1a248fe29265375ee1106c2fc5783f
Kirby McKenzie <kirby@workplacearcade.com>
2021-05-19 09:38:55 +1000

    Style the reset password page and move logic into smaller components

--- components/ResetPasswordForm/ResetPasswordForm.js
+++ components/ResetPasswordForm/ResetPasswordForm.js
@@ -1,6 +1,5 @@
 import React from "react";
 import PropTypes from "prop-types";
-
 import * as Yup from "yup";
 
 import {
@@ -15,22 +14,30 @@ import {
   FormLabel,
   Image,
   Input,
+  Link as ChakraLink,
   Text,
   ScaleFade,
 } from "@chakra-ui/react";
 import { Formik, Field, Form } from "formik";
-import Link from "next/link";
 
 import SolidSuccessCircle from "../../Icons/SolidSuccessCircle";
+import SolidSubtractCircle from "../../Icons/SolidSubtractCircle";
 
-const forgotPasswordExplainer =
-  "Enter the email address you used when you joined and we’ll send you a link to reset your password.";
+import Link from "next/link";
 
 const validationSchema = Yup.object().shape({
-  email: Yup.string().required("Please include an email."),
+  password: Yup.string()
+    .required("Please include a password.")
+    .matches(
+      /(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/,
+      "Must contain at least 8 characters, one uppercase letter, one lowercase letter and one number."
+    ),
 });
 
-const ForgotPasswordForm = ({ error, hasResetPassword, onSubmit }) => {
+const resetPasswordExplainer =
+  "Enter your new password. Make sure it's secure and different to your last one.";
+
+const ResetPasswordForm = ({ error, isSuccess, onSubmit }) => {
   const successMessage = (
     <Fade in out>
       <Flex marginBottom={4} alignItems="center" direction="column">
@@ -42,25 +49,70 @@ const ForgotPasswordForm = ({ error, hasResetPassword, onSubmit }) => {
             color="green.500"
           />
         </ScaleFade>
-        <Text textAlign="center" fontWeight="500" fontSize="14px">
-          {"Successfully sent password reset link. Please check your email."}
+        <Text textAlign="center" fontWeight="bold" marginBottom={1}>
+          {"Successfully updated password!"}
+        </Text>
+
+        <Text color="gray.600" textAlign="center" fontSize="14px">
+          {"Please "}
+          <Link href="/login">
+            <ChakraLink
+              fontWeight={600}
+              _hover={{ textDecoration: "underline" }}
+            >
+              {"login"}
+            </ChakraLink>
+          </Link>
+          {" to continue."}
         </Text>
       </Flex>
     </Fade>
   );
 
+  const errorMessage = (
+    <Flex marginBottom={4} alignItems="center" direction="column">
+      <ScaleFade initialScale={0.5} in>
+        <SolidSubtractCircle
+          marginBottom={2}
+          height="60px"
+          width="56px"
+          color="red.500"
+        />
+      </ScaleFade>
+      <Text
+        textAlign="center"
+        fontSize="14px"
+        fontWeight="bold"
+        marginBottom={2}
+      >
+        {error}
+      </Text>
+
+      <Link href="/forgot-password">
+        <ChakraLink
+          fontWeight={600}
+          color="gray.600"
+          textAlign="center"
+          fontSize="14px"
+          _hover={{ textDecoration: "underline" }}
+        >
+          {"request new token"}
+        </ChakraLink>
+      </Link>
+    </Flex>
+  );
+
   const form = (
     <>
       <Text fontSize="26px" marginY={2} fontWeight="800">
-        {"Forgotten Password"}
+        {"Reset Your Password"}
       </Text>
 
       <Text marginTop={2} color="gray.600" fontSize="14px">
-        {forgotPasswordExplainer}
+        {resetPasswordExplainer}
       </Text>
-
       <Formik
-        initialValues={{ email: "" }}
+        initialValues={{ password: "" }}
         validationSchema={validationSchema}
         onSubmit={(values, actions) => {
           onSubmit(values);
@@ -69,21 +121,20 @@ const ForgotPasswordForm = ({ error, hasResetPassword, onSubmit }) => {
       >
         {(formProps) => (
           <Form>
-            <Field name="email">
+            <Field name="password">
               {({ field, form }) => (
                 <FormControl
                   marginY={6}
-                  isInvalid={form.errors.email && form.touched.email}
+                  isInvalid={form.errors.password && form.touched.password}
                 >
-                  <FormLabel htmlFor="email" hidden>
-                    {"Email"}
+                  <FormLabel htmlFor="password" hidden>
+                    {"Password"}
                   </FormLabel>
-
                   <Input
                     {...field}
-                    id="email"
-                    type="email"
-                    placeholder="Enter email..."
+                    id="password"
+                    type="password"
+                    placeholder="Enter new password..."
                     size="lg"
                     fontSize="16px"
                     background="#F6F6F6"
@@ -91,10 +142,9 @@ const ForgotPasswordForm = ({ error, hasResetPassword, onSubmit }) => {
                     _placeholder={{ color: "gray.500" }}
                     _hover={{ background: "#e0e0e0" }}
                   />
-
                   <Box position="absolute" top="38px" left="2px">
                     <FormErrorMessage fontSize="11px">
-                      {form.errors.email}
+                      {form.errors.password}
                     </FormErrorMessage>
                   </Box>
                 </FormControl>
@@ -143,6 +193,17 @@ const ForgotPasswordForm = ({ error, hasResetPassword, onSubmit }) => {
     </>
   );
 
+  const getViewComponent = () => {
+    if (error) {
+      return errorMessage;
+    }
+    if (isSuccess) {
+      return successMessage;
+    }
+
+    return form;
+  };
+
   return (
     <Flex
       marginTop={6}
@@ -171,21 +232,21 @@ const ForgotPasswordForm = ({ error, hasResetPassword, onSubmit }) => {
           </Link>
         </Flex>
 
-        {hasResetPassword ? successMessage : form}
+        {getViewComponent()}
       </Flex>
     </Flex>
   );
 };
 
-ForgotPasswordForm.propTypes = {
+ResetPasswordForm.propTypes = {
   error: PropTypes.string,
-  hasResetPassword: PropTypes.bool,
+  isSuccess: PropTypes.bool,
   onSubmit: PropTypes.func,
 };
-ForgotPasswordForm.defaultProps = {
+ResetPasswordForm.defaultProps = {
   error: "",
-  hasResetPassword: false,
+  isSuccess: false,
   onSubmit: () => {},
 };
 
-export default ForgotPasswordForm;
+export default ResetPasswordForm;
---
+++ containers/ResetPasswordContainer/ResetPasswordContainer.js
@@ -0,0 +1,77 @@
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+
+import axiosClient from "../../axios/axiosClient";
+import useCurrentUser from "../../hooks/UseCurrentUser";
+
+import ResetPasswordForm from "../../components/ResetPasswordForm";
+
+const ResetPasswordContainer = () => {
+  const router = useRouter();
+  const { userId, token } = router.query;
+
+  const { user, isLoading: isLoadingUser } = useCurrentUser();
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  // TODO: update name
+  const [error, setError] = useState(null);
+  const [isSuccess, setIsSuccess] = useState(false);
+
+  // TODO: break into separate useEffects
+  useEffect(() => {
+    if (!isLoadingUser && user) {
+      router.push("/");
+    }
+
+    if (userId && token) {
+      axiosClient
+        .get(`/auth/reset-token-valid/${userId}/${token}`)
+        .then((response) => {
+          if (!response.data) {
+            setError(
+              "Invalid reset token. Please request another token and try again."
+            );
+          }
+          setIsLoading(false);
+        })
+        .catch((error) => {
+          setError(error.response.data);
+          setIsLoading(false);
+        });
+    }
+  }, [userId, token, user, isLoadingUser]);
+
+  const handleSubmit = ({ password }) => {
+    setError(null);
+    axiosClient
+      .put("/auth", {
+        userId: parseInt(userId),
+        token,
+        password,
+      })
+      .then(() => {
+        setIsSuccess(true);
+      })
+      .catch((error) => {
+        setError(error.response.data);
+      });
+  };
+
+  if (isLoading || isLoadingUser || user) {
+    return null;
+  }
+
+  return (
+    <ResetPasswordForm
+      error={error}
+      isSuccess={isSuccess}
+      onSubmit={handleSubmit}
+    />
+  );
+};
+
+ResetPasswordContainer.propTypes = {};
+ResetPasswordContainer.defaultProps = {};
+
+export default ResetPasswordContainer;
