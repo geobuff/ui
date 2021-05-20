aa40c0ee848c93d3670560e4600204f38a328e12
Kirby McKenzie <kirby.mckenzie@hotmail.com>
2021-05-20 11:00:04 +1000

    Update reset password to use mobile view logic

--- pages/reset-password/[userId]/[token].js
+++ pages/reset-password/[userId]/[token].js
@@ -1,17 +1,35 @@
 import React from "react";
 import Head from "next/head";
 
+import { useBreakpointValue } from "@chakra-ui/react";
+
 import MainView from "../../../components/MainView";
 import ResetPasswordContainer from "../../../containers/ResetPasswordContainer";
 
 const ResetPassword = () => {
+  const backgroundColor = useBreakpointValue({ base: "#FFF", md: "#F0F0F0" });
+
+  // Stops page flickering while it figures out backgroundColor
+  if (!backgroundColor) {
+    return null;
+  }
+
   return (
-    <MainView hasNavigationBar={false} footerVariant="simple">
+    <>
       <Head>
         <title>Reset Password - GeoBuff</title>
       </Head>
-      <ResetPasswordContainer />
-    </MainView>
+
+      <style jsx global>{`
+        body {
+          background: ${backgroundColor};
+        }
+      `}</style>
+
+      <MainView hasNavigationBar={false} footerVariant="simple">
+        <ResetPasswordContainer />
+      </MainView>
+    </>
   );
 };
 
