473be423b810e7dc7ea394144e7eb00bd715ecb8
Kirby McKenzie <kirby.mckenzie@hotmail.com>
2021-06-18 12:20:29 +1000

    Update GameBottomSheetModal to snap to lowest snapPoint on game start

--- components/GameBottomSheetModal/GameBottomSheetModal.js
+++ components/GameBottomSheetModal/GameBottomSheetModal.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import PropTypes from "prop-types";
 import Link from "next/link";
 import {
@@ -32,89 +32,103 @@ const GameBottomSheetModal = ({
   isOpen,
   onGameStart,
   onGameStop,
-}) => (
-  <Box
-    as={Sheet}
-    isOpen={isOpen}
-    snapPoints={snapPoints}
-    initialSnap={initialSnap}
-    mt="120px"
-    top="100% !important"
-    minHeight="92vh"
-    springConfig={{
-      stiffness: 600,
-      damping: 60,
-      mass: 0.2,
-    }}
-  >
-    <Sheet.Container style={{ position: "fixed" }}>
-      <Box pt={1} height="54px" as={Sheet.Header} />
-      <Sheet.Content>
-        <Flex
-          direction="column"
-          height="100%"
-          overflowY="scroll"
-          mx={5}
-          my={0}
-          pb="100px"
-        >
-          <Box>
-            <Heading pt={0} size="md">
-              <Flex justifyContent="center">
-                {quiz.hasLeaderboard && (
-                  <Link href={`/leaderboard?quizId=${quiz.id}`}>
-                    <ChakraLink>
-                      <Twemoji emoji="🏆" mr={2} />
-                    </ChakraLink>
-                  </Link>
-                )}
-                {quiz.name}
-              </Flex>
-            </Heading>
+}) => {
+  const ref = useRef();
+
+  // Push modal down on game start
+  useEffect(() => {
+    if (hasGameStarted) {
+      snapTo(snapPoints.length - 1);
+    }
+  }, [hasGameStarted]);
+
+  const snapTo = (snapIndex) => ref.current?.snapTo(snapIndex);
+
+  return (
+    <Box
+      ref={ref}
+      as={Sheet}
+      isOpen={isOpen}
+      snapPoints={snapPoints}
+      initialSnap={initialSnap}
+      mt="120px"
+      top="100% !important"
+      minHeight="92vh"
+      springConfig={{
+        stiffness: 600,
+        damping: 60,
+        mass: 0.2,
+      }}
+    >
+      <Sheet.Container style={{ position: "fixed" }}>
+        <Box pt={1} height="54px" as={Sheet.Header} />
+        <Sheet.Content>
+          <Flex
+            direction="column"
+            height="100%"
+            overflowY="scroll"
+            mx={5}
+            my={0}
+            pb="100px"
+          >
+            <Box>
+              <Heading pt={0} size="md">
+                <Flex justifyContent="center">
+                  {quiz.hasLeaderboard && (
+                    <Link href={`/leaderboard?quizId=${quiz.id}`}>
+                      <ChakraLink>
+                        <Twemoji emoji="🏆" mr={2} />
+                      </ChakraLink>
+                    </Link>
+                  )}
+                  {quiz.name}
+                </Flex>
+              </Heading>
+
+              <Divider my={4} />
+
+              <Box my={4}>
+                <Button
+                  colorScheme={hasGameStarted ? "red" : "green"}
+                  isFullWidth
+                  onClick={hasGameStarted ? onGameStop : onGameStart}
+                  p={8}
+                  size="md"
+                >
+                  <Text fontWeight="700" fontSize="22px">
+                    {hasGameStarted
+                      ? "GIVE UP"
+                      : hasGameRunOnce
+                      ? "RETRY"
+                      : "START"}
+                  </Text>
+                </Button>
+              </Box>
+            </Box>
 
             <Divider my={4} />
 
-            <Box my={4}>
-              <Button
-                colorScheme={hasGameStarted ? "red" : "green"}
-                isFullWidth
-                onClick={hasGameStarted ? onGameStop : onGameStart}
-                p={8}
-                size="md"
-              >
-                <Text fontWeight="700" fontSize="22px">
-                  {hasGameStarted
-                    ? "GIVE UP"
-                    : hasGameRunOnce
-                    ? "RETRY"
-                    : "START"}
-                </Text>
-              </Button>
+            <Box mt={4}>
+              <Text fontWeight="bold" mb={1}>
+                {"RECENT"}
+              </Text>
+              <ResultsList quiz={quiz} results={recents} />
             </Box>
-          </Box>
 
-          <Divider my={4} />
-
-          <Box mt={4}>
-            <Text fontWeight="bold" mb={1}>
-              {"RECENT"}
-            </Text>
-            <ResultsList quiz={quiz} results={recents} />
-          </Box>
-
-          <ResultsMap
-            quiz={quiz}
-            checked={checked}
-            map={groupMapping(mapping)}
-            hasGameStopped={hasGameStopped}
-            hasGroupings={quiz.hasGrouping}
-            hasFlags={quiz.hasFlags}
-          />
-        </Flex>
-      </Sheet.Content>
-    </Sheet.Container>
-  </Box>
-);
+            <ResultsMap
+              quiz={quiz}
+              checked={checked}
+              map={groupMapping(mapping)}
+              hasGameStopped={hasGameStopped}
+              hasGroupings={quiz.hasGrouping}
+              hasFlags={quiz.hasFlags}
+            />
+          </Flex>
+        </Sheet.Content>
+      </Sheet.Container>
+    </Box>
+  );
+};
 
 GameBottomSheetModal.propTypes = {
   quiz: PropTypes.shape({
