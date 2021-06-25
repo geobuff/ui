7daf14602c6e53879e26ad843010bc16579c69c0
Kirby McKenzie <kirby.mckenzie@hotmail.com>
2021-06-25 15:46:42 +1000

    Add loading spinner to GameMapQuizContainer to handle longer load times

--- containers/GameMapQuizContainer/GameMapQuizContainer.js
+++ containers/GameMapQuizContainer/GameMapQuizContainer.js
@@ -6,12 +6,25 @@ import GameMapQuiz from "../../components/GameMapQuiz";
 import useQuiz from "../../hooks/UseQuiz";
 import useMapping from "../../hooks/UseMapping";
 
+import { Fade, Flex, Spinner } from "@chakra-ui/react";
+
 const GameMapQuizContainer = ({ quizId }) => {
   const { quiz, isLoading: isLoadingQuiz } = useQuiz(quizId);
   const { mapping, isLoading: isLoadingMapping } = useMapping(quizId);
 
   if (isLoadingQuiz || isLoadingMapping) {
-    return null;
+    return (
+      <Flex
+        height="90vh"
+        alignItems="center"
+        justifyContent="center"
+        color="white"
+      >
+        <Fade in unmountOnExit>
+          <Spinner size="xl" color="#1d8db3" />
+        </Fade>
+      </Flex>
+    );
   }
 
   return <GameMapQuiz quiz={quiz} mapping={mapping} map={Maps[quiz.mapSVG]} />;
