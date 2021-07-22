41c7cccb822a02aca4afa2e591088c5b69663fdd
Ash Midgley <ashleymidgley@protonmail.com>
2021-07-22 14:14:41 +1200

    Add spinner to flag game on initial load.

--- containers/GameMapQuizContainer/GameMapQuizContainer.js
+++ containers/GameMapQuizContainer/GameMapQuizContainer.js
@@ -3,28 +3,16 @@ import PropTypes from "prop-types";
 import * as Maps from "@geobuff/maps";
 
 import GameMapQuiz from "../../components/GameMapQuiz";
+import GameSpinner from "../../components/GameSpinner";
 import useQuiz from "../../hooks/UseQuiz";
 import useMapping from "../../hooks/UseMapping";
-
-import { Fade, Flex, Spinner } from "@chakra-ui/react";
 
 const GameMapQuizContainer = ({ quizId }) => {
   const { quiz, isLoading: isLoadingQuiz } = useQuiz(quizId);
   const { mapping, isLoading: isLoadingMapping } = useMapping(quizId);
 
   if (isLoadingQuiz || isLoadingMapping) {
-    return (
-      <Flex
-        height={{ base: "30vh", md: "90vh" }}
-        alignItems="center"
-        justifyContent="center"
-        color="white"
-      >
-        <Fade in unmountOnExit>
-          <Spinner size="xl" color="#1d8db3" />
-        </Fade>
-      </Flex>
-    );
+    return <GameSpinner />;
   }
 
   return <GameMapQuiz quiz={quiz} mapping={mapping} map={Maps[quiz.mapSVG]} />;
