36f604f202dfd93aa75191a17abfbea9324227d6
Kirby McKenzie <kirby.mckenzie@hotmail.com>
2021-09-12 16:50:24 +1000

    Remove unneeded width on mobile GameFlags row and reduce paddings on grid

--- components/GameFlags/GameFlags.tsx
+++ components/GameFlags/GameFlags.tsx
@@ -22,7 +22,7 @@ const GameFlags: FC<Props> = ({
       {isMobile ? (
         <Box as={ScrollingComponent} overflowX="auto">
           <Flex
-            width="1525px"
+            width="100%"
             position="relative"
             height="100px"
             marginRight={10}
@@ -48,7 +48,7 @@ const GameFlags: FC<Props> = ({
           justifyContent="center"
           paddingLeft="390px"
         >
-          <SimpleGrid columns={5} spacingX={8} spacingY={6}>
+          <SimpleGrid columns={5} spacingX={6} spacingY={6}>
             {[...Array.from(new Set(codes))]?.map((code) => (
               <DraggableFlag
                 key={code}
