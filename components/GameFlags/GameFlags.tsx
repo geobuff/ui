a17dd646431f8aad97ffbf1c78387030e4497941
Kirby McKenzie <kirby.mckenzie@hotmail.com>
2021-10-14 15:44:35 +1100

    Fix issue where having less items than the carousel row count would cause the flags to disappear

--- components/GameFlags/GameFlags.tsx
+++ components/GameFlags/GameFlags.tsx
@@ -1,52 +1,18 @@
 import React, { FC, useContext } from "react";
-import {
-  Box,
-  Button,
-  Flex,
-  SimpleGrid,
-  useBreakpointValue,
-} from "@chakra-ui/react";
-import withScrolling from "react-dnd-scrolling";
+import { Flex, SimpleGrid, useBreakpointValue } from "@chakra-ui/react";
 
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
-
-// TODO: km - we do still need this?
-const ScrollingComponent = withScrolling("div");
 
 import DraggableFlag from "../DraggableFlag";
 import DraggableFlagPreview from "../DraggableFlag/DraggableFlagPreview";
 import CarouselButton from "../Carousel/CarouselButton";
 import { FlagGameContext } from "../../context/FlagGameContext";
-import { CurrentUserContext } from "../../context/CurrentUserContext";
 
 interface Props {
   codes?: string[];
   onCheckSubmission?: (submission: string) => void;
 }
-
-const responsive = {
-  tablet: {
-    breakpoint: { max: 1000, min: 600 },
-    items: 5,
-    slidesToSlide: 3,
-  },
-  largerMobile: {
-    breakpoint: { max: 600, min: 500 },
-    items: 4,
-    slidesToSlide: 2,
-  },
-  mobile: {
-    breakpoint: { max: 500, min: 370 },
-    items: 3,
-    slidesToSlide: 2,
-  },
-  smallMobile: {
-    breakpoint: { max: 370, min: 0 },
-    items: 2,
-    slidesToSlide: 1,
-  },
-};
 
 const GameFlags: FC<Props> = ({
   codes = [],
@@ -55,6 +21,31 @@ const GameFlags: FC<Props> = ({
   const isMobile = useBreakpointValue({ base: true, lg: false });
 
   const { dragItem } = useContext(FlagGameContext);
+
+  const mobileItems = codes.length > 3 ? 3 : codes.length;
+
+  const responsive = {
+    tablet: {
+      breakpoint: { max: 1000, min: 600 },
+      items: 5,
+      slidesToSlide: 3,
+    },
+    largerMobile: {
+      breakpoint: { max: 600, min: 500 },
+      items: 4,
+      slidesToSlide: 2,
+    },
+    mobile: {
+      breakpoint: { max: 500, min: 370 },
+      items: mobileItems,
+      slidesToSlide: 2,
+    },
+    smallMobile: {
+      breakpoint: { max: 370, min: 0 },
+      items: 2,
+      slidesToSlide: 1,
+    },
+  };
 
   return (
     <>
@@ -72,13 +63,13 @@ const GameFlags: FC<Props> = ({
             <Carousel
               ssr
               infinite
-              centerMode
+              centerMode={codes.length >= 3}
               responsive={responsive}
               deviceType={"mobile"}
               customTransition="transform 150ms ease-in-out"
               customLeftArrow={<CarouselButton position="left" />}
               customRightArrow={<CarouselButton position="right" />}
-              itemClass="flex align-center"
+              itemClass="flex center"
             >
               {[...Array.from(new Set(codes))]?.map((code) => (
                 <DraggableFlag
@@ -92,8 +83,6 @@ const GameFlags: FC<Props> = ({
           </Flex>
         </>
       ) : (
-        // </Flex>
-        // </Box>
         <Flex
           minWidth="300px"
           width="100%"
