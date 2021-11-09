ccf5b547862f93488334aa52a4dad93d2763bb47
Kirby McKenzie <kirby.mckenzie@hotmail.com>
2021-11-10 10:57:23 +1100

    Update QuizList placeholder to render more placeholders on bigger screens

--- containers/QuizListContainer/QuizListContainer.tsx
+++ containers/QuizListContainer/QuizListContainer.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from "react";
+import { use100vh } from "react-div-100vh";
 
 import QuizList from "../../components/QuizList";
 import useQuizzes from "../../hooks/UseQuizzes";
@@ -8,11 +9,32 @@ interface Props {
   filter?: string;
 }
 
+const getNumberOfTilesByHeight = (height: number): number => {
+  // Ensure we always have something rendering immediately
+  if (height === null) {
+    return 20;
+  }
+
+  switch (true) {
+    case height >= 1300:
+      return 20;
+    case height < 1300 && height >= 1000:
+      return 15;
+    default:
+      return 10;
+  }
+};
+
 const QuizListContainer: FC<Props> = ({ filter = "" }) => {
   const { quizzes, isLoading } = useQuizzes(filter);
 
+  const height = use100vh();
+  const noOfTiles = getNumberOfTilesByHeight(height);
+
+  console.log(height, "height");
+
   if (isLoading) {
-    return <QuizListPlaceholder noOfTiles={8} />;
+    return <QuizListPlaceholder noOfTiles={noOfTiles} />;
   }
 
   return <QuizList quizzes={quizzes} />;
