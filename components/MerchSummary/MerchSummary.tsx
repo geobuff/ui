f36daf3ea8e412a7884f6c5b1cea36a53190fa43
Ash Midgley <ashleymidgley@protonmail.com>
2021-11-27 11:01:49 +1300

    Refactor merch summary into smaller components.

--- components/MerchSummary/MerchSummary.tsx
+++ components/MerchSummary/MerchSummary.tsx
@@ -1,31 +1,12 @@
-import React, { FC, useState } from "react";
-import Link from "next/link";
-import * as Yup from "yup";
-import { Field, Form, Formik } from "formik";
-
-import {
-  Box,
-  Button,
-  Flex,
-  FormControl,
-  FormErrorMessage,
-  Heading,
-  Select,
-  SimpleGrid,
-  Text,
-  useDisclosure,
-} from "@chakra-ui/react";
+import React, { FC } from "react";
+import { Flex, useDisclosure } from "@chakra-ui/react";
 
 import Image from "../Image";
 import Modal from "../Modal";
 import { MerchItem } from "../../types/merch-item";
 import { MerchSummaryFormSubmit } from "../../types/merch-summary-form-submit";
-import MapInteractionCSS from "../MapInteractionCSS";
-import { useRouter } from "next/router";
-
-const validationSchema = Yup.object().shape({
-  size: Yup.string().required("Please choose an available size."),
-});
+import MerchSummaryShowcase from "./MerchSummaryShowcase";
+import MerchSummaryDetails from "./MerchSummaryDetails";
 
 interface Props {
   item?: MerchItem;
@@ -40,11 +21,7 @@ const MerchSummary: FC<Props> = ({
   onSubmit = (values: MerchSummaryFormSubmit): void => {},
   submitted = false,
 }) => {
-  const router = useRouter();
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const [currentImage, setCurrentImage] = useState(
-    item?.images.find((x) => x.isPrimary).imageUrl
-  );
 
   return (
     <>
@@ -57,27 +34,7 @@ const MerchSummary: FC<Props> = ({
       >
         <Flex justifyContent="center">
           <Flex direction="column">
-            <MapInteractionCSS background="#F0F0F0" borderRadius="12px">
-              <Image
-                src={currentImage}
-                width="500px"
-                height="auto"
-                borderRadius="12px"
-              />
-            </MapInteractionCSS>
-            <SimpleGrid mt={6} columns={{ base: 3, md: 4 }} spacingY={6}>
-              {item?.images.map((image) => (
-                <Image
-                  key={image.id}
-                  src={image.imageUrl}
-                  onClick={(): void => setCurrentImage(image.imageUrl)}
-                  width="100px"
-                  height="auto"
-                  borderRadius="12px"
-                  cursor="pointer"
-                />
-              ))}
-            </SimpleGrid>
+            <MerchSummaryShowcase images={item?.images} />
           </Flex>
         </Flex>
         <Flex
@@ -86,90 +43,19 @@ const MerchSummary: FC<Props> = ({
           px={{ base: 0, lg: 12 }}
           mt={{ base: 12, lg: 0 }}
         >
-          <Heading>{item?.name}</Heading>
-          <Text mt={3}>{`$${item?.price.Float64}`}</Text>
-          <Text mt={3}>{item?.description}</Text>
-          <Formik
-            initialValues={{
-              size: "",
-            }}
-            validationSchema={validationSchema}
+          <MerchSummaryDetails
+            name={item?.name}
+            price={item?.price.Float64}
+            description={item?.description}
+            sizeGuideImageUrl={
+              item?.sizeGuideImageUrl.Valid && item?.sizeGuideImageUrl.String
+            }
+            sizes={item?.sizes}
             onSubmit={onSubmit}
-          >
-            {(): React.ReactNode => (
-              <Form>
-                <Flex
-                  direction={{ base: "column", xl: "row" }}
-                  justifyContent={{ base: "center", xl: "space-between" }}
-                  marginY={6}
-                >
-                  <Flex minWidth={item?.sizeGuideImageUrl.Valid && "65%"}>
-                    <Field name="size">
-                      {({ field, form }): React.ReactNode => (
-                        <FormControl
-                          isInvalid={form.errors.size && form.touched.size}
-                        >
-                          <Select {...field}>
-                            <option value="" disabled>
-                              Select a size...
-                            </option>
-                            {item?.sizes.map((size) => (
-                              <option
-                                key={size.id}
-                                value={size.id}
-                                disabled={size.quantity === 0}
-                              >
-                                {size.size}{" "}
-                                {size.quantity === 0 && " - Sold Out"}
-                              </option>
-                            ))}
-                          </Select>
-                          <Box position="absolute" top="38px" left="2px">
-                            <FormErrorMessage fontSize="11px">
-                              {form.errors.size}
-                            </FormErrorMessage>
-                          </Box>
-                        </FormControl>
-                      )}
-                    </Field>
-                  </Flex>
-                  {item?.sizeGuideImageUrl.Valid && (
-                    <Flex
-                      direction="column"
-                      justifyContent="center"
-                      mt={{ base: 9, xl: 0 }}
-                    >
-                      <Button onClick={onOpen}>Size Guide</Button>
-                    </Flex>
-                  )}
-                </Flex>
-
-                <Button
-                  width="100%"
-                  mt={3}
-                  onClick={() => router.push("/merch")}
-                >
-                  {"Continue Shopping"}
-                </Button>
-                <Button
-                  colorScheme="green"
-                  width="100%"
-                  type="submit"
-                  isLoading={isSubmitting}
-                  mt={3}
-                >
-                  {"Add To Cart"}
-                </Button>
-              </Form>
-            )}
-          </Formik>
-          {submitted && (
-            <Link href="/shopping-cart">
-              <Button colorScheme="teal" width="100%" mt={3}>
-                View Cart
-              </Button>
-            </Link>
-          )}
+            isSubmitting={isSubmitting}
+            submitted={submitted}
+            onOpen={onOpen}
+          />
         </Flex>
       </Flex>
       <Modal
