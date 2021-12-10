9f2e60f7bd577617223550a8640adc9e8d743901
Ash Midgley <ashleymidgley@gmail.com>
2021-12-10 18:50:43 +1300

    Cancel order on checkout cancel redirect.

--- pages/checkout/canceled.tsx
+++ pages/checkout/canceled.tsx
@@ -1,17 +1,27 @@
-import React, { useEffect, FC } from "react";
+import React, { useEffect, FC, useState } from "react";
 import { useRouter } from "next/router";
 import { Text } from "@chakra-ui/react";
+import axiosClient from "../../axios";
 
 const Canceled: FC = () => {
   const router = useRouter();
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    setTimeout(() => {
-      router.push("/shopping-cart");
-    }, 3000);
+    const email = router.query.email as string;
+    axiosClient.delete(`/orders/${email}`).finally(() => {
+      setIsLoading(false);
+      setTimeout(() => {
+        router.push("/shopping-cart");
+      }, 2000);
+    });
   }, [router]);
 
-  return <Text m={3}>Transaction cancelled. Redirecting back to cart...</Text>;
+  if (isLoading) {
+    return <Text m={3}>Cancelling transaction...</Text>;
+  }
+
+  return <Text m={3}>Redirecting back to cart...</Text>;
 };
 
 export default Canceled;
