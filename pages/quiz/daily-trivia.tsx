926e711b5abaed675dbd18d07129c7395f6370d1
Kirby McKenzie <kirby.mckenzie@hotmail.com>
2021-12-24 08:41:09 +1100

    push quiz buttons to bottom of page

--- pages/quiz/daily-trivia.tsx
+++ pages/quiz/daily-trivia.tsx
@@ -1,11 +1,12 @@
 import React, { FC } from "react";
-import { Box, Button, Heading, SimpleGrid } from "@chakra-ui/react";
+import { Box, Button, Flex, Heading, SimpleGrid } from "@chakra-ui/react";
 import MainView from "../../components/MainView";
 
 export interface Props {}
 
 const QuizButton: FC = ({ children }) => (
   <Button
+    textAlign="left"
     backgroundColor="#236175"
     color="white"
     size="lg"
@@ -20,17 +21,32 @@ const QuizButton: FC = ({ children }) => (
 const DailyTrivia: FC<Props> = () => {
   return (
     <MainView hasFooter={false} backgroundColor="#276F86">
-      <Box width="100%" maxWidth={1300} marginLeft="auto" marginRight="auto">
+      <Flex
+        flex={1}
+        direction="column"
+        height="100%"
+        width="100%"
+        maxWidth={1300}
+        marginLeft="auto"
+        marginRight="auto"
+      >
         <Heading color="white">
           {"If I’m visiting the ancient city of Petra, which country am I in?"}
         </Heading>
-        <SimpleGrid marginY={4} columns={2} spacing={4}>
-          <QuizButton>{"Peru"}</QuizButton>
-          <QuizButton>{"New Zealand"}</QuizButton>
-          <QuizButton>{"Jordan"}</QuizButton>
-          <QuizButton>{"United Arab Emirates"}</QuizButton>
-        </SimpleGrid>
-      </Box>
+        <Flex
+          width="100%"
+          direction="column"
+          marginTop="auto"
+          marginBottom={12}
+        >
+          <SimpleGrid columns={2} spacing={4}>
+            <QuizButton>{"Peru"}</QuizButton>
+            <QuizButton>{"New Zealand"}</QuizButton>
+            <QuizButton>{"Jordan"}</QuizButton>
+            <QuizButton>{"United Arab Emirates"}</QuizButton>
+          </SimpleGrid>
+        </Flex>
+      </Flex>
     </MainView>
   );
 };
