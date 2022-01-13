c3d170c989d3e2dc5d3741dfd58ca4188cb035d9
Kirby McKenzie <kirby.mckenzie@hotmail.com>
2022-01-14 09:43:13 +1100

    Revert gtag changes

--- helpers/gtag.ts
+++ helpers/gtag.ts
@@ -1,13 +1,10 @@
-const isDevelopment = process.env.NODE_ENV === "development";
-
 export const pageview = (url: URL): void => {
-  if (isDevelopment || typeof window !== "undefined") {
-    return;
+  if (typeof window !== "undefined") {
+    //@ts-ignore
+    window?.gtag("config", process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
+      page_path: url,
+    });
   }
-  //@ts-ignore
-  window?.gtag("config", process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
-    page_path: url,
-  });
 };
 
 type GTagEvent = {
@@ -18,13 +15,12 @@ type GTagEvent = {
 };
 
 export const event = ({ action, category, label, value }: GTagEvent): void => {
-  if (isDevelopment || typeof window !== "undefined") {
-    return;
+  if (typeof window !== "undefined") {
+    //@ts-ignore
+    window?.gtag("event", action, {
+      event_category: category,
+      event_label: label,
+      value,
+    });
   }
-  //@ts-ignore
-  window?.gtag("event", action, {
-    event_category: category,
-    event_label: label,
-    value,
-  });
 };
