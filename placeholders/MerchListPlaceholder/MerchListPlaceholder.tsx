b74b5a83d8c638acf0a8df745b5404a66aefb6fc
Kirby McKenzie <kirby.mckenzie@hotmail.com>
2022-01-16 20:44:16 +1100

    Update MerchListPlaceholder to be same size as MerchList cards

--- placeholders/MerchListPlaceholder/MerchListPlaceholder.tsx
+++ placeholders/MerchListPlaceholder/MerchListPlaceholder.tsx
@@ -23,7 +23,7 @@ const MerchListPlaceholder: FC<Props> = ({ noOfTiles }) => (
       {[...Array(noOfTiles)].map((_, i) => (
         <Skeleton
           key={i}
-          width={{ base: "180px", md: "220px" }}
+          width={{ base: "225px", md: "250px" }}
           height={{ base: "225px", md: "250px" }}
           borderRadius={12}
         ></Skeleton>
