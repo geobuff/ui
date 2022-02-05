1d3e60305f563477fb99018c3fa82b8b2a4c7db6
Ash Midgley <ashleymidgley@gmail.com>
2022-02-05 13:37:53 +1300

    Add tabs. Add basic placeholders to users components.

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,7 +1,23 @@
 import React, { FC } from "react";
 import Head from "next/head";
+
+import {
+  Flex,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+} from "@chakra-ui/react";
+
 import MainView from "../components/MainView";
-import AdminContainer from "../containers/AdminContainer";
+import AdminTotalUserCountContainer from "../containers/AdminTotalUserCountContainer";
+import AdminUsersContainer from "../containers/AdminUsersContainer";
+import AdminTopFiveQuizPlaysContainer from "../containers/AdminTopFiveQuizPlaysContainer";
+import AdminLastWeekTriviaPlaysContainer from "../containers/AdminLastWeekTriviaPlaysContainer";
+import AdminQuizzesContainer from "../containers/AdminQuizzesContainer";
+import AdminOrdersContainer from "../containers/AdminOrdersContainer";
+import AdminDiscountsContainer from "../containers/AdminDiscountsContainer";
 
 const Admin: FC = () => (
   <>
@@ -9,7 +25,68 @@ const Admin: FC = () => (
       <title>{"Admin Dashboard - GeoBuff"}</title>
     </Head>
     <MainView>
-      <AdminContainer />
+      <Tabs>
+        <TabList>
+          <Tab>Users</Tab>
+          <Tab>Quizzes</Tab>
+          <Tab>Trivia</Tab>
+          <Tab>Orders</Tab>
+        </TabList>
+
+        <TabPanels>
+          <TabPanel>
+            <Flex justifyContent="center">
+              <Flex
+                direction="column"
+                height="100%"
+                width="100%"
+                maxWidth={1300}
+              >
+                <AdminTotalUserCountContainer />
+                <AdminUsersContainer />
+              </Flex>
+            </Flex>
+          </TabPanel>
+          <TabPanel>
+            <Flex justifyContent="center">
+              <Flex
+                direction="column"
+                height="100%"
+                width="100%"
+                maxWidth={1300}
+              >
+                <AdminTopFiveQuizPlaysContainer />
+                <AdminQuizzesContainer />
+              </Flex>
+            </Flex>
+          </TabPanel>
+          <TabPanel>
+            <Flex justifyContent="center">
+              <Flex
+                direction="column"
+                height="100%"
+                width="100%"
+                maxWidth={1300}
+              >
+                <AdminLastWeekTriviaPlaysContainer />
+              </Flex>
+            </Flex>
+          </TabPanel>
+          <TabPanel>
+            <Flex justifyContent="center">
+              <Flex
+                direction="column"
+                height="100%"
+                width="100%"
+                maxWidth={1300}
+              >
+                <AdminOrdersContainer />
+                <AdminDiscountsContainer />
+              </Flex>
+            </Flex>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
     </MainView>
   </>
 );
