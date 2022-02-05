fe9f29655905eb9147a044cf72a334f73c4887f7
Ash Midgley <ashleymidgley@gmail.com>
2022-02-05 14:08:25 +1300

    Add placeholder for admin trivia.

---
+++ placeholders/AdminLastWeekTriviaPlaysPlaceholder/AdminLastWeekTriviaPlaysPlaceholder.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from "react";
+import { Skeleton } from "@chakra-ui/react";
+
+const AdminLastWeekTriviaPlaysPlaceholder: FC = () => (
+  <Skeleton height="400px" margin={6} borderRadius={12} />
+);
+
+export default AdminLastWeekTriviaPlaysPlaceholder;
