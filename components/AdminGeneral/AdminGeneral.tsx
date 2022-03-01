ad5170207a1fc25a9820d3c019516c8a1eaa155c
Kirby McKenzie <kirby.mckenzie@hotmail.com>
2022-03-01 18:28:30 +1100

    Plish UI of AdminGeneral and refactor container to handle more tasks while keeping dry

--- components/AdminGeneral/AdminGeneral.tsx
+++ components/AdminGeneral/AdminGeneral.tsx
@@ -1,20 +1,65 @@
 import React, { FC } from "react";
-import { Alert, AlertIcon, Button, Flex, Stack } from "@chakra-ui/react";
+import {
+  Alert,
+  AlertIcon,
+  Button,
+  Divider,
+  Flex,
+  Heading,
+  Text,
+} from "@chakra-ui/react";
+import Card from "../Card";
+import { BackgroundTaskKey } from "../../types/background-task";
+
+const {
+  DeployDevWeb,
+  DeployProdAll,
+  DeployProdMobile,
+  DeployProdWeb,
+  TriviaCreate,
+} = BackgroundTaskKey;
+
+const deploymentControls = [
+  {
+    key: DeployProdAll,
+    title: "Deploy All Prod UI",
+    subtitle: "Redeploys both mobile-geobuff.vercel.app and geobuff.com",
+    buttonText: "Deploy",
+  },
+  {
+    key: DeployProdWeb,
+    title: "Deploy Prod UI",
+    subtitle: "Redeploys geobuff.com",
+    buttonText: "Deploy",
+  },
+  {
+    key: DeployProdMobile,
+    title: "Deploy Mobile UI",
+    subtitle: "Redeploys mobile-geobuff.vercel.app",
+    buttonText: "Deploy",
+  },
+  {
+    key: DeployDevWeb,
+    title: "Deploy Dev UI",
+    subtitle: "Redeploys dev.geobuff.com",
+    buttonText: "Deploy",
+  },
+  {
+    key: TriviaCreate,
+    title: "Create Trivia",
+    subtitle: "Generates the daily trivia for tomorrow",
+    buttonText: "Generate",
+  },
+];
 
 export interface Props {
-  onDevUIDeploy?: () => void;
-  onProdUIDeploy?: () => void;
-  onMobileProdUIDeploy?: () => void;
-  onCreateTrivia: () => void;
+  onDeploy: (key: BackgroundTaskKey) => void;
   isSubmitting?: boolean;
   error?: string;
 }
 
 const AdminGeneral: FC<Props> = ({
-  onDevUIDeploy = (): void => {},
-  onProdUIDeploy = (): void => {},
-  onMobileProdUIDeploy = (): void => {},
-  onCreateTrivia = (): void => {},
+  onDeploy = () => {},
   isSubmitting = false,
   error = "",
 }) => {
@@ -26,28 +71,39 @@ const AdminGeneral: FC<Props> = ({
           {error}
         </Alert>
       )}
-      <Flex
-        margin={6}
-        padding={12}
-        background="white"
-        borderRadius={12}
-        justifyContent="center"
-      >
-        <Stack>
-          <Button onClick={onDevUIDeploy} disabled={isSubmitting}>
-            Deploy Dev UI
-          </Button>
-          <Button onClick={onProdUIDeploy} disabled={isSubmitting}>
-            Deploy Prod UI
-          </Button>
-          <Button onClick={onMobileProdUIDeploy} disabled={isSubmitting}>
-            Deploy Mobile Prod UI
-          </Button>
-          <Button onClick={onCreateTrivia} disabled={isSubmitting}>
-            Create Trivia
-          </Button>
-        </Stack>
-      </Flex>
+      <Card marginY={10} padding={6}>
+        <Heading size="lg" marginBottom={5}>
+          {"General"}
+        </Heading>
+
+        {deploymentControls.map((control) => (
+          <Flex key={control.key} direction="column">
+            <Divider borderWidth={1} my={2} />
+            <Flex
+              direction="row"
+              justifyContent="space-between"
+              alignItems="center"
+              marginY={2}
+              marginX={1}
+            >
+              <Flex direction="column">
+                <Text fontSize={18} fontWeight="bold">
+                  {control.title}
+                </Text>
+                <Text color="gray.500">{control.subtitle}</Text>
+              </Flex>
+              <Button
+                onClick={() => onDeploy(control.key)}
+                disabled={isSubmitting}
+                colorScheme="teal"
+                width="100px"
+              >
+                {control.buttonText}
+              </Button>
+            </Flex>
+          </Flex>
+        ))}
+      </Card>
     </>
   );
 };
