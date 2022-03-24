63759b476833f02598044dec5b32e50f6bd99bc3
Kirby McKenzie <kirby.mckenzie@hotmail.com>
2022-03-24 20:58:11 +1100

    Refactor field logic into CommunityQuizFormField

--- components/CommunityQuizForm/CommunityQuizForm.tsx
+++ components/CommunityQuizForm/CommunityQuizForm.tsx
@@ -1,13 +1,8 @@
 import React, { FC } from "react";
-import {
-  Flex,
-  FormControl,
-  FormErrorMessage,
-  FormHelperText,
-  FormLabel,
-  Input,
-} from "@chakra-ui/react";
-import { Field, Formik } from "formik";
+import { Formik } from "formik";
+import { Flex } from "@chakra-ui/react";
+
+import CommunityQuizFormField from "./CommunityQuizFormField";
 
 export interface FormValues {
   quizName: string;
@@ -22,46 +17,20 @@ const initialValues = { quizName: "" };
 const CommunityQuizForm: FC<Props> = ({ values = initialValues }) => {
   return (
     <Formik initialValues={values} onSubmit={(values) => console.log(values)}>
-      <Flex width="100%">
-        <Field name="quizName">
-          {({ field, form }) => (
-            <FormControl
-              isInvalid={form.errors.question && form.touched.question}
-            >
-              <Flex>
-                <Flex width="50%" direction="column">
-                  <FormLabel
-                    htmlFor="question"
-                    fontWeight="bold"
-                    marginBottom={0.5}
-                  >
-                    {"Quiz Name"}
-                  </FormLabel>
-                  <FormHelperText marginTop={0}>
-                    {"Keep it concise and memorable!"}
-                  </FormHelperText>
-                </Flex>
-                <Input
-                  width="50%"
-                  {...field}
-                  id="quizName"
-                  type="text"
-                  placeholder="Enter quiz name..."
-                  size="lg"
-                  fontSize="16px"
-                  fontWeight={400}
-                  background="#F6F6F6"
-                  borderRadius={6}
-                  _placeholder={{ color: "gray.500" }}
-                  _hover={{ background: "#e0e0e0" }}
-                />
-              </Flex>
-              <FormErrorMessage fontSize="11px">
-                {form.errors.quizName}
-              </FormErrorMessage>
-            </FormControl>
-          )}
-        </Field>
+      <Flex direction="column" width="100%">
+        <CommunityQuizFormField
+          name="quizName"
+          label="Quiz Name"
+          helper="Keep it concise and memorable!"
+          placeholder="Enter quiz name..."
+        />
+        {/* TODO: add textarea type */}
+        <CommunityQuizFormField
+          name="description"
+          label="Description"
+          helper="The description helps your quiz stand out from the rest"
+          placeholder="Enter description..."
+        />
       </Flex>
     </Formik>
   );
