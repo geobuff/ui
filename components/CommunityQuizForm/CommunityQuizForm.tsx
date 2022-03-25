62d8d0e3a89e8f0c48a2c1d045fcae98b8f6ab47
Kirby McKenzie <kirby.mckenzie@hotmail.com>
2022-03-26 10:48:13 +1100

    Wireup Modal to Add question button

--- components/CommunityQuizForm/CommunityQuizForm.tsx
+++ components/CommunityQuizForm/CommunityQuizForm.tsx
@@ -1,9 +1,11 @@
 import React, { FC } from "react";
 import { Formik } from "formik";
-import { Button, Divider, Flex, Text } from "@chakra-ui/react";
+import { Button, Divider, Flex, useDisclosure } from "@chakra-ui/react";
 
 import CommunityQuizFormField from "./CommunityQuizFormField";
 import CommunityQuizQuestionsField from "./CommunityQuizQuestionsField";
+import Modal from "../Modal";
+import AdminManualTriviaQuestionForm from "../AdminManualTriviaQuestionForm";
 
 export interface FormValues {
   quizName: string;
@@ -16,41 +18,61 @@ export interface Props {
 const initialValues = { quizName: "", description: "" };
 
 const CommunityQuizForm: FC<Props> = ({ values = initialValues }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
-    <Formik
-      initialValues={values}
-      onSubmit={(values) => console.log(values, "values")}
-    >
-      <Flex direction="column" width="100%">
-        <CommunityQuizFormField
-          name="quizName"
-          label="Quiz Name"
-          helper="Keep it concise and memorable!"
-          placeholder="Enter quiz name..."
-        />
-        {/* TODO: add textarea type */}
-        <CommunityQuizFormField
-          name="description"
-          label="Description"
-          helper="The description helps your quiz stand out from the rest"
-          placeholder="Enter description..."
-        />
+    <>
+      <Formik
+        initialValues={values}
+        onSubmit={(values) => console.log(values, "values")}
+      >
+        <Flex direction="column" width="100%">
+          <CommunityQuizFormField
+            name="quizName"
+            label="Quiz Name"
+            helper="Keep it concise and memorable!"
+            placeholder="Enter quiz name..."
+          />
+          {/* TODO: add textarea type */}
+          <CommunityQuizFormField
+            name="description"
+            label="Description"
+            helper="The description helps your quiz stand out from the rest"
+            placeholder="Enter description..."
+          />
 
-        <Divider my={5} borderColor="gray.100" borderWidth={1} />
+          <Divider my={5} borderColor="gray.100" borderWidth={1} />
 
-        <Flex width="100%" justifyContent="center" marginY={4}>
-          <CommunityQuizQuestionsField />
+          <Flex width="100%" justifyContent="center" marginY={4}>
+            <CommunityQuizQuestionsField
+              questions={[]}
+              onAddQuestion={onOpen}
+            />
+          </Flex>
+
+          <Divider my={5} borderColor="gray.100" borderWidth={1} />
+
+          <Flex justifyContent="flex-end" marginTop={8}>
+            <Button type="submit" colorScheme="green">
+              {"Create Quiz"}
+            </Button>
+          </Flex>
         </Flex>
+      </Formik>
 
-        <Divider my={5} borderColor="gray.100" borderWidth={1} />
-
-        <Flex justifyContent="flex-end" marginTop={8}>
-          <Button type="submit" colorScheme="green">
-            {"Create Quiz"}
-          </Button>
+      <Modal
+        isOpen={isOpen}
+        onClose={onClose}
+        maxHeight={{ base: "100%", md: "700px" }}
+        minWidth="660px"
+        hasCloseButton
+        header={"Add Question"}
+      >
+        <Flex paddingX={10} width="100%" overflow="scroll">
+          <AdminManualTriviaQuestionForm />
         </Flex>
-      </Flex>
-    </Formik>
+      </Modal>
+    </>
   );
 };
 
--- components/CommunityQuizForm/CommunityQuizQuestionsField/CommunityQuizQuestionsField.tsx
+++ components/CommunityQuizForm/CommunityQuizQuestionsField/CommunityQuizQuestionsField.tsx
@@ -1,15 +1,47 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import { Button, Flex, Text } from "@chakra-ui/react";
 
-// export interface Props {}
+// TODO: inherit from a common shape
+export interface CommunityQuizAnswer {
+  text: string;
+  isCorrect: boolean;
+  flagCode: string;
+}
 
-const CommunityQuizQuestionsField: FC = () => {
+export interface CommunityQuizQuestion {
+  type: string;
+  imageUrl?: string;
+  flagCode?: string;
+  map?: string;
+  highlighted?: string;
+  question: string;
+  answers: CommunityQuizAnswer[];
+}
+
+export interface Props {
+  questions: CommunityQuizQuestion[];
+  onAddQuestion?: () => void;
+}
+
+const CommunityQuizQuestionsField: FC<Props> = ({
+  questions = [],
+  onAddQuestion = () => {},
+}) => {
   return (
     <Flex direction="column">
-      <Text color="gray.400" fontWeight="medium" marginBottom={4}>
-        {"No questions added"}
-      </Text>
-      <Button variant="outline" colorScheme="blue" maxWidth="200px">
+      {!questions.length ? (
+        <Text color="gray.400" fontWeight="medium" marginBottom={4}>
+          {"No questions added"}
+        </Text>
+      ) : (
+        <Flex>{questions}</Flex>
+      )}
+      <Button
+        variant="outline"
+        colorScheme="blue"
+        maxWidth="200px"
+        onClick={onAddQuestion}
+      >
         {"Add Question"}
       </Button>
     </Flex>
