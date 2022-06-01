be480d3a973ad3c57479496197158af9cc845501
Ash Midgley <ash@Ashs-MacBook-Pro.local>
2022-06-01 16:12:37 +1200

    Add empty state to admin merch, users, quizzes and manual trivia questions tables.

--- components/AdminMerchTable/AdminMerchTable.tsx
+++ components/AdminMerchTable/AdminMerchTable.tsx
@@ -1,5 +1,7 @@
 import React, { FC } from "react";
 import {
+  Alert,
+  AlertIcon,
   Box,
   Divider,
   Flex,
@@ -20,51 +22,64 @@ export interface Props {
   isLoading?: boolean;
 }
 
-const AdminMerchTable: FC<Props> = ({ merch = [], isLoading = true }) => (
-  <Card marginY={10} padding={6}>
-    <Flex
-      justifyContent="space-between"
-      alignItems="center"
-      marginBottom={5}
-      marginX={2}
-    >
-      <Heading fontSize="24px">{"Inventory"}</Heading>
-    </Flex>
+const AdminMerchTable: FC<Props> = ({ merch = [], isLoading = true }) => {
+  const getTable = (): JSX.Element => {
+    if (merch.length === 0) {
+      return (
+        <Alert status="info" borderRadius={6} marginBottom={3}>
+          <AlertIcon />
+          No merch to display.
+        </Alert>
+      );
+    }
 
-    <Divider borderWidth={1} marginBottom={4} />
+    return (
+      <Table size="md" variant="striped" colorscheme="gray">
+        <Thead>
+          <Tr>
+            <Th textAlign="left">{"SIZE"}</Th>
+            <Th textAlign="right">{"PRICE"}</Th>
+            <Th textAlign="right">{"QUANTITY"}</Th>
+          </Tr>
+        </Thead>
 
-    <Box overflow="auto" margin={6}>
-      {isLoading ? (
-        <AdminMerchTablePlaceholder />
-      ) : (
-        <Table size="md" variant="striped" colorscheme="gray">
-          <Thead>
-            <Tr>
-              <Th textAlign="left">{"SIZE"}</Th>
-              <Th textAlign="right">{"PRICE"}</Th>
-              <Th textAlign="right">{"QUANTITY"}</Th>
+        <Tbody>
+          {merch.map((item, index) => (
+            <Tr key={index} fontWeight={600}>
+              <TableCell paddingY={3} paddingX={6}>
+                {`${item.name} - ${item.size}`}
+              </TableCell>
+              <TableCell isNumeric paddingY={3} paddingX={6}>
+                {item.price.Valid && item.price.Float64}
+              </TableCell>
+              <TableCell isNumeric paddingY={3} paddingX={6}>
+                {item.quantity}
+              </TableCell>
             </Tr>
-          </Thead>
+          ))}
+        </Tbody>
+      </Table>
+    );
+  };
 
-          <Tbody>
-            {merch.map((item, index) => (
-              <Tr key={index} fontWeight={600}>
-                <TableCell paddingY={3} paddingX={6}>
-                  {`${item.name} - ${item.size}`}
-                </TableCell>
-                <TableCell isNumeric paddingY={3} paddingX={6}>
-                  {item.price.Valid && item.price.Float64}
-                </TableCell>
-                <TableCell isNumeric paddingY={3} paddingX={6}>
-                  {item.quantity}
-                </TableCell>
-              </Tr>
-            ))}
-          </Tbody>
-        </Table>
-      )}
-    </Box>
-  </Card>
-);
+  return (
+    <Card marginY={10} padding={6}>
+      <Flex
+        justifyContent="space-between"
+        alignItems="center"
+        marginBottom={5}
+        marginX={2}
+      >
+        <Heading fontSize="24px">{"Inventory"}</Heading>
+      </Flex>
+
+      <Divider borderWidth={1} marginBottom={4} />
+
+      <Box overflow="auto" margin={6}>
+        {isLoading ? <AdminMerchTablePlaceholder /> : getTable()}
+      </Box>
+    </Card>
+  );
+};
 
 export default AdminMerchTable;
