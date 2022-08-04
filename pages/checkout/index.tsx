6ccead7df51c15498fadd77f9a8f167aad0459a1
Ash Midgley <ashleymidgley@gmail.com>
2022-08-05 10:46:59 +1200

    Fix checkout error due to maps api script.

--- pages/checkout/index.tsx
+++ pages/checkout/index.tsx
@@ -5,11 +5,15 @@ import MainView from "../../components/MainView";
 import CheckoutFormContainer from "../../containers/CheckoutFormContainer";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js/pure";
+import useScript from "../../hooks/UseScript";
 
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
 
 const Checkout: FC = () => {
   const ref = useRef(null);
+  const status = useScript(
+    `https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&libraries=places`
+  );
 
   return (
     <>
@@ -19,14 +23,11 @@ const Checkout: FC = () => {
           name="description"
           content="Add your details so we can ship you GeoBuff merch straight to your doorstep."
         />
-        <script
-          src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&libraries=places`}
-        />
       </Head>
       <MainView innerRef={ref}>
         <HeroHeader heading="Checkout" />
         <Elements stripe={stripePromise}>
-          <CheckoutFormContainer />
+          <CheckoutFormContainer isMapsApiLoading={status !== "ready"} />
         </Elements>
       </MainView>
     </>
