a46e16a89acec855383984e7a7bdb976e9d9aeba
Ash Midgley <ashleymidgley@protonmail.com>
2022-11-09 16:49:11 +0800

    Remove import

--- components/AuthErrorRedirect/AuthErrorRedirect.tsx
+++ components/AuthErrorRedirect/AuthErrorRedirect.tsx
@@ -1,7 +1,6 @@
 import { FC, useEffect } from "react";
 import { useSession, signOut } from "next-auth/react";
 import { useRouter } from "next/router";
-import { useToast } from "@chakra-ui/react";
 
 interface Props {
   children?: JSX.Element;
