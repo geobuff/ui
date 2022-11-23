5add4a1431aec800ddde3d8872b532bc52d0a567
Ash Midgley <ashleymidgley@protonmail.com>
2022-11-24 11:51:05 +1300

    Fix get local storage item for axiosClient.

--- axios/axiosClient.tsx
+++ axios/axiosClient.tsx
@@ -7,7 +7,10 @@ const axiosClient = axios.create({
 axiosClient.interceptors.request.use(async (config) => {
   config.headers = {
     ...config.headers,
-    "Content-Language": localStorage.getItem("geobuff.language") || "en",
+    "Content-Language":
+      (typeof window !== "undefined" &&
+        window.localStorage.getItem("geobuff.language")) ||
+      "en",
   };
 
   return config;
