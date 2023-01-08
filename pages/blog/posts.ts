fcf21ef63f88571785dd821325e1bdb5962a2062
Ash Midgley <ashleymidgley@protonmail.com>
2023-01-08 17:07:06 +1300

    Readd blog boilerplate and template first post.

--- pages/blog/posts.ts
+++ pages/blog/posts.ts
@@ -1,1 +1,9 @@
-export default [];
+export default [
+  {
+    date: "9 January, 2023",
+    slug: "russia-european-or-asian-nation",
+    title: "Russia: European or Asian nation?",
+    description:
+      "Russia is a transcontinental country located in Eastern Europe and Northern Asia. The Ural Mountains, which run through western Russia, are traditionally considered the dividing line between the two continents...",
+  },
+];
