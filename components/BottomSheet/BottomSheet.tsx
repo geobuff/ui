946fd52d69c52c1c019773e7a0ad1cc50cd0b715
Ash Midgley <ashleymidgley@protonmail.com>
2023-01-30 16:21:46 +1300

    Fix build of BottomSheet.

--- components/BottomSheet/BottomSheet.tsx
+++ components/BottomSheet/BottomSheet.tsx
@@ -1,7 +1,35 @@
 import React, { FC, createRef, useEffect } from "react";
 
 import { Box } from "@chakra-ui/react";
-import Sheet from "react-modal-sheet";
+import dynamic from "next/dynamic";
+
+const Sheet = dynamic(
+  () => import("react-modal-sheet").then((mod) => mod.default),
+  {
+    ssr: false,
+  }
+);
+
+const Container = dynamic(
+  () => import("react-modal-sheet").then((mod) => mod.default.Container),
+  {
+    ssr: false,
+  }
+);
+
+const Content = dynamic(
+  () => import("react-modal-sheet").then((mod) => mod.default.Content),
+  {
+    ssr: false,
+  }
+);
+
+const Header = dynamic(
+  () => import("react-modal-sheet").then((mod) => mod.default.Header),
+  {
+    ssr: false,
+  }
+);
 
 const snapPoints = [600, 400, 300, 90];
 const initialSnap = snapPoints.length - 2;
@@ -19,11 +47,12 @@ export const BottomSheet: FC<BottomSheetProps> = ({
 }) => {
   const ref = createRef<any>();
 
-  useEffect(() => {
-    if (isMinimised) {
-      ref?.current?.snapTo(snapPoints.length - 1);
-    }
-  }, [isMinimised, ref]);
+  // TODO - Reintroduce.
+  // useEffect(() => {
+  //   if (isMinimised) {
+  //     ref?.current?.snapTo(snapPoints.length - 1);
+  //   }
+  // }, [isMinimised, ref]);
 
   return (
     <Box
@@ -43,8 +72,8 @@ export const BottomSheet: FC<BottomSheetProps> = ({
       }}
       onClose={(): void => {}}
     >
-      <Sheet.Container style={{ position: "fixed" }}>
-        <Box as={Sheet.Header} />
+      <Container style={{ position: "fixed" }}>
+        <Box as={Header} />
         <Box
           margin="auto"
           borderRadius={25}
@@ -55,8 +84,8 @@ export const BottomSheet: FC<BottomSheetProps> = ({
           marginTop={-4}
         />
 
-        <Sheet.Content>{children}</Sheet.Content>
-      </Sheet.Container>
+        <Content>{children}</Content>
+      </Container>
     </Box>
   );
 };
